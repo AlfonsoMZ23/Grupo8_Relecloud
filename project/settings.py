c4689950a761d701a21c8afefe671d6c2c32f6f2
AlfonsoMZ23 <alfonso.moreno.shape@gmail.com>
2023-12-18 20:49:41 +0100

    Debug false, migrations aplicadas

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+%zemdq*5_ax99c1y%ol20nevwkgd*r3pf15^c6urbq2)(d6&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'alfonso-moreno-relecloud.azurewebsites.net',
