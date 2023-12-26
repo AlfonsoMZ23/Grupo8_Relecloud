b546072e2f72f2ca6590064c8d0dca982c09832e
hugogarcia <hugo.hg361@gmail.com>
2023-12-26 20:34:38 +0100

    Merge pull request #1 from AlfonsoMZ23/develop
    hugo commit

--- relecloud/views.py
+++ relecloud/views.py
@@ -57,14 +57,8 @@ from django.shortcuts import render, redirect
 from .forms import OpinionForm
 from .models import Opinion
 
-def opiniones(request):
-    opinions = Opinion.objects.all()  # Obtener todas las opiniones existentes
-    form = OpinionForm()
+# views.py
+from django.shortcuts import render, redirect
+from .forms import OpinionForm
+from .models import Opinion
 
-    if request.method == 'POST':
-        form = OpinionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('opiniones')  # Redireccionar después de enviar el formulario
-
-    return render(request, 'tu_app/opiniones.html', {'opinions': opinions, 'form': form})
